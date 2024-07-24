8743acb843e503a9e99dab91325cb30f76e61f78
derjungeinhd <125304376+derjungeinhd@users.noreply.github.com>
2024-07-24 23:33:15 +0200

    update Enums

--- src/main/java/de/derjungeinhd/tinytoolbox/hideandseek/SeekTimerRunnable.java
+++ src/main/java/de/derjungeinhd/tinytoolbox/hideandseek/SeekTimerRunnable.java
@@ -31,13 +31,13 @@ public class SeekTimerRunnable extends BukkitRunnable{
         for (String pname : this.hnsManager.getHiders()) {
             Player p = Bukkit.getPlayerExact(pname);
             if (p != null) {
-                p.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 40, 255, false, false));
+                p.addPotionEffect(new PotionEffect(PotionEffectType.RESISTANCE, 40, 255, false, false));
             }
         }
         for (String pname : this.hnsManager.getSeekers()) {
             Player p = Bukkit.getPlayerExact(pname);
             if (p != null) {
-                p.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 40, 255, false, false));
+                p.addPotionEffect(new PotionEffect(PotionEffectType.RESISTANCE, 40, 255, false, false));
                 p.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 40, 0, false, false));
                 p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 40, 2, false, false));
             }
